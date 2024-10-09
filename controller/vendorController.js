70e589faca771fe2c3e546f63511e21bd36141df
codeuniquecode <uniquestha619@gmail.com>
2024-10-09 14:42:20 +0545

    implemented settings, logout and edit profile for vendor dashboard

--- controller/vendorController.js
+++ controller/vendorController.js
@@ -77,3 +77,61 @@ exports.addProduct =[
         res.render('addProduct',{message:'Product added successfully'});
     }
 ]
+exports.renderEditProfile = (req,res)=>{
+    res.render('editVendorProfile');
+}
+exports.editProfile = async(req,res)=>{
+    const vendorId = req.user;
+    if(!vendorId){
+        return res.send('Vendor does not exist');
+    }   
+    const {phone,email,address} = req.body;
+    const update = await vendor.update({
+        phonenumber:phone,
+        email,
+        address
+    },{
+        where:{
+            vendorId
+        }
+    });
+    if(!update){
+        return res.send('Profile not updated');
+    }
+    res.render('editVendorProfile',{message:'Profile updated successfully'});
+}
+exports.logout = (req,res)=>{
+    res.clearCookie('token');
+    res.redirect('/');
+}
+exports.renderChangePassword = (req,res)=>{
+    res.render('vendorChangepass.ejs');
+}
+exports.changePassword = async(req,res)=>{
+    const vendorId = req.user;
+    const {old_password,new_password} = req.body;
+    if(new_password.length<8){
+        return res.send('password must be atleast 8 characters long');
+    }
+    const vendorData = await vendor.findOne({
+        where:{
+            vendorId
+        }
+    })
+    const isMatch = bcrypt.compareSync(old_password,vendorData.password);
+    if(isMatch){
+        const update = await vendor.update({
+            password:bcrypt.hashSync(new_password,1)
+        },{
+            where:{
+                vendorId
+            }
+        });
+        if(update){
+            return res.redirect('/login');
+        }
+    }
+    else{
+        return res.send('old password doesnot match,please try again');
+    }
+}
--- routes/vendorRoutes.js
+++ routes/vendorRoutes.js
@@ -10,7 +10,12 @@ router.get('/forget', vendorController.forget);
 router.get('/vendorRegister', vendorController.showVendorRegister); // Use the renamed function
 router.get('/vendordashboard',isAuthenticated, vendorController.vendorDashboard);
 router.get('/addProduct',isAuthenticated, vendorController.renderAddProduct);
+router.get('/editProfile',isAuthenticated, vendorController.renderEditProfile);
+router.get('/logout', vendorController.logout);
+router.get('/changePassword',isAuthenticated, vendorController.renderChangePassword);
 // POST
 router.post('/vendorRegister', vendorController.vendorRegister);
 router.post('/addProduct',isAuthenticated, vendorController.addProduct);
+router.post('/editVendor',isAuthenticated, vendorController.editProfile);
+router.post('/vendorChangePassword',isAuthenticated, vendorController.changePassword);
 module.exports = router;
