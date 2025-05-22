b8d81335fbc1d845bcc7e8e672ced979fd98065b
Unique Shrestha <uniquestha619@gmail.com>
2025-05-22 22:23:48 +0545

    test

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 app.use(express.static('storage'));
+app.use('/storage', express.static('storage'));
 
 app.use(express.urlencoded({extended:true})); 
 app.use(cookieParser());
