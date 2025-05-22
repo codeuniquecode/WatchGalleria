5d61bc12f36694abb9d322ea03ec6571993ca56a
Unique Shrestha <uniquestha619@gmail.com>
2025-05-22 20:52:42 +0545

    chaging port

--- model/index.js
+++ model/index.js
@@ -17,7 +17,7 @@ require('dotenv').config();
     // const sequelize = new Sequelize('railway','root','btQBUtaDKrZYuaTUquXSPtqTzNPjWEOS',{
       const sequelize = new Sequelize(dbase, user, password, {
         host: host,
-        port: 29525, // 3306 for local
+        port: 54014, // 3306 for local
         dialect: 'mysql',
         pool: {
             max: 10,
